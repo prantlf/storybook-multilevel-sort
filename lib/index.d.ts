c9e5ef1b808bbe52a2226e7a3f570009e3a3f494
Ferdinand Prantl <prantlf@gmail.com>
2022-05-08 10:15:01 +0200

    feat: Add TypeScript types

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+interface Order {
+  [key: string]: Order | null
+}
+
+// eslint-disable-next-line no-explicit-any
+type Story = any
+
+declare function sort(order: Order, story1: Story, story2: Story): 0 | -1 | 1
+
+export default sort
