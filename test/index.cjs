b67cfef9203890c63a9eff55779c93ecc50df43c
Ferdinand Prantl <prantlf@gmail.com>
2022-11-26 15:29:52 +0100

    fix: Support browsers without Object.hasOwn
    Reported on Safari 14 (#6).

--- test/index.cjs
+++ test/index.cjs
@@ -1,7 +1,17 @@
+delete Object.hasOwn // simulate older browsers
+
 const { equal } = require('assert')
 const test = require('tehanu')(__filename)
 const sort = require('../lib/index.cjs')
 
 test('exports a function', () => {
   equal(typeof sort, 'function')
+})
+
+test('supports browsers without Object.hasOwn', () => {
+  const story1 = [null, { kind: '', name: 'Story1' }]
+  const story2 = [null, { kind: '', name: 'Story2' }]
+  const order = { '': { story2: {}, story1: {} } };
+  equal(sort(order, story1, story2), 1)
+  equal(sort(order, story2, story1), -1)
 })
