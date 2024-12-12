fbd0f6504a7d1fb2c6e22a762e2537aa2750f615
Ferdinand Prantl <prantlf@gmail.com>
2024-12-13 00:33:37 +0100

    fix: Sort properly if not every story group contains autodocs story
    Sort by the story type (`docs` or `story`) only if the two stories
    are in the same group. Stories in different groups should be sorted
    according to the configuration (or alphabetically), regardless
    of their type.
    
    Fixes #12

--- test/index.js
+++ test/index.js
@@ -232,6 +232,13 @@ test('puts docs before stories by default', () => {
   strictEqual(compareStories(null, story, docs), 1)
 })
 
+test('puts docs before only if the stories are in the sdame group', () => {
+  const docs = { title: 'Group 2', name: 'Docs', type: 'docs' }
+  const story = { title: 'Group 1', name: 'Story', type: 'story' }
+  strictEqual(compareStories(null, docs, story), 1)
+  strictEqual(compareStories(null, story, docs), -1)
+})
+
 test('puts stories before docs when just story order is specified', () => {
   const docs = { title: '', name: 'Docs', type: 'docs' }
   const story = { title: '', name: 'Story', type: 'story' }
